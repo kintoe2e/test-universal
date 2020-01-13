a63beb396c021bea6f1bf714cb97ea8f11208fc2
Nandi Wong <nandi@kintohub.com>
2020-01-13 14:24:48 +0800

    add callback to the function

--- static-website-build-dockerfile/build.js
+++ static-website-build-dockerfile/build.js
@@ -1,9 +1,16 @@
 const mkdirp = require('mkdirp');
 const fs = require('fs');
 
-mkdirp('./build')
+mkdirp('./build', (err) => {
+  if (err) {
+    console.error(err);
+    throw (error)
+  }
 
-let content = fs.readFileSync('./web/index.html').toString();
-content = content.replace('Anonymous', process.env.USERNAME || 'Nobody')
+  let content = fs.readFileSync('./web/index.html').toString();
+  content = content.replace('Anonymous', process.env.USERNAME || 'Nobody')
 
-fs.writeFileSync('./build/index.html', content);
+  fs.writeFileSync('./build/index.html', content);
+})
+
+
